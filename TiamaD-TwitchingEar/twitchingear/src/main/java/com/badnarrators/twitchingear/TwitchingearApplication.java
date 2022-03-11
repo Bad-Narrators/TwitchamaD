4a9a1c460fd5f8243855fca1068f5d92801c6b72
BadNarrators <andreaatzei01@gmail.com>
2022-03-11 16:58:35 +0100

    TwitchingEar: Initialized Spring TwitchingEar

---
+++ TiamaD-TwitchingEar/twitchingear/src/main/java/com/badnarrators/twitchingear/TwitchingearApplication.java
@@ -0,0 +1,13 @@
+package com.badnarrators.twitchingear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitchingearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitchingearApplication.class, args);
+	}
+
+}
