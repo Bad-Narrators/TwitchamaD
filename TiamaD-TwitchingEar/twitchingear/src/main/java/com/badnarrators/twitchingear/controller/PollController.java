338ae888d75e8257764aa56e5c3062e5b500ef1e
LuciusHeit <andreaatzei01@gmail.com>
2022-04-02 05:20:48 +0200

    Basic setup for testing for Poll

--- TiamaD-TwitchingEar/twitchingear/src/main/java/com/badnarrators/twitchingear/controller/PollController.java
+++ TiamaD-TwitchingEar/twitchingear/src/main/java/com/badnarrators/twitchingear/controller/PollController.java
@@ -1,5 +1,6 @@
 package com.badnarrators.twitchingear.controller;
 
+import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicLong;
 
 import com.badnarrators.twitchingear.model.PollResponse;
@@ -15,15 +16,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/poll")
 public class PollController {
     
-    private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping(value="/get")
     @ResponseBody
     public PollResponse response(@RequestParam(required = false) String id) {
         if(id == null) {
-            return new PollResponse ("0", "This value was null");
+            return new PollResponse ();
         }
-        return new PollResponse(id, String.format(template, id));
+
+        //Test variables, will be changed later
+        String question = "Vecchia Londra o Messicano?";
+        ArrayList<String> answers = new ArrayList<String>();
+        answers.add("Vecchia Londra");
+        answers.add("Messicano");
+        ArrayList<Integer> votes = new ArrayList<Integer>();
+        votes.add(250);
+        votes.add(12);
+
+        return new PollResponse(id, question, answers, votes);
     }
 }
--- TiamaD-TwitchingEar/twitchingear/src/main/java/com/badnarrators/twitchingear/model/PollResponse.java
+++ TiamaD-TwitchingEar/twitchingear/src/main/java/com/badnarrators/twitchingear/model/PollResponse.java
@@ -1,20 +1,45 @@
 package com.badnarrators.twitchingear.model;
 
+import java.util.ArrayList;
+
+import org.apache.tomcat.jni.Poll;
+
 public class PollResponse {
     
     private final String id;
-    private final String content;
+    private final String question;
+    private final ArrayList<String> answers;
+    private final ArrayList<Integer> votes;
 
-    public PollResponse(String id, String content) {
-        this.id = id;
-        this.content = content;
+    public PollResponse() {
+        this.id = "0";
+        this.question = "Missing ID in request";
+        this.answers = new ArrayList<>();
+        this.votes = new ArrayList<>();
     }
+
+    public PollResponse(String id, String question, ArrayList<String> answers, ArrayList<Integer> votes) {
+        this.id = id;
+        this.question = question;
+        this.answers = answers;
+        this.votes = votes;
+    }
+
 
     public String getId() {
         return id;
     }
 
-    public String getContent() {
-        return content;
+    public String getQuestion() {
+        return question;
     }
+
+    public ArrayList<String> getAnswers() {
+        return answers;
+    }
+
+    public ArrayList<Integer> getVotes() {
+        return votes;
+    }
+
 }
