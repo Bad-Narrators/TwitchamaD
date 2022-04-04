a595cddbc69fc7526fd4ed841f8446c7e6886368
BadNarrators <andreaatzei01@gmail.com>
2022-04-04 17:50:32 +0200

    moved samples

---
+++ TiamaD-TwitchingEar/twitchingear/src/main/java/com/badnarrators/twitchingear/common/model/SampleResponse.java
@@ -0,0 +1,20 @@
+package com.badnarrators.twitchingear.common.model;
+
+public class SampleResponse {
+    
+    private final long id;
+    private final String content;
+
+    public SampleResponse(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
