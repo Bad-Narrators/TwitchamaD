e15c724731f570cd28cd8c1574ab0355b2657bdb
LuciusHeit <andreaatzei01@gmail.com>
2022-04-15 00:48:01 +0200

    PollBot - poll commands vote and (get)poll created

--- TiamaD-Bot/main.py
+++ TiamaD-Bot/main.py
@@ -6,15 +6,10 @@ import requests
 
 class Bot(commands.Bot):
 
-    def __init__(self, token):
-        # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
-        # prefix can be a callable, which returns a list of strings or a string...
-        # initial_channels can also be a callable which returns a list of strings...
-        super().__init__(token=token, prefix='?', initial_channels=['luciusheit'])
+    def __init__(self, token, prefix, initChannels):
+        super().__init__(token=token, prefix=prefix, initial_channels=initChannels)
 
     async def event_ready(self):
-        # Notify us when everything is ready!
-        # We are logged in and ready to chat and use commands...
         print(f'Logged in as | {self.nick}')
         print(f'User id is | {self.user_id}')
 
@@ -30,46 +25,40 @@ class Bot(commands.Bot):
         # Since we have commands and are overriding the default `event_message`
         # We must let the bot know we want to handle and invoke our commands...
         await self.handle_commands(message)
-
-    @commands.command()
-    async def hello(self, ctx: commands.Context):
-        # Here we have a command hello, we can invoke our command with our prefix and command name
-        # e.g ?hello
-        # We can also give our commands aliases (different names) to invoke with.
-
-        # Send a hello back!
-        # Sending a reply back to the channel is easy... Below is an example.
-        await ctx.send(f'Hello {ctx.author.name}!')
-
-    # twitch command to create a poll
-    @commands.command()
-    async def createPoll(self, ctx: commands.Context, *, question):
-        url = "http:localhost:8080/api/poll/getByQuestion?question="+question
-        response = requests.get(url)
-        if response.status_code == 200:
-            await ctx.send(f'Poll already exists!')
-        else:
-            url = "http:localhost:8080/api/poll/create?question="+question
-            response = requests.get(url)
-            if response.status_code == 200:
-                await ctx.send(f'Poll created!')
-            else:
-                await ctx.send(f'Poll not created!')
-        await ctx.send(f'Poll: {question}')
+        
 
     # twitch command to add a vote to a poll
     @commands.command()
-    async def vote(self, ctx: commands.Context, *, vote):
-        url = "http:localhost:8080/api/poll/vote?question="+vote
+    async def vote(self, ctx: commands.Context, *, poll="0"):
+        url = "http:localhost:8080/api/poll/vote?poll="+poll
+        print(f'Request to {url}')
         response = requests.get(url)
         if response.status_code == 200:
-            await ctx.send(f'Vote added!')
+            print(f'Vote added to poll {poll} | Status code: {response.status_code}')
+            await ctx.send(f'Thanks for voting!')
         else:
+            print(f'Vote not added to poll {poll} | Status code: {response.status_code}')
             await ctx.send(f'Vote not added!')
-        await ctx.send(f'Vote: {vote}')
+        print(f'{url}')
+
+    # twitch command to show the current poll
+    @commands.command()
+    async def poll(self, ctx: commands.Context, poll="0"):
+        url = "http:localhost:8080/api/poll/getPollString?poll="+poll
+        print(f'Request to {url}')
+        response = requests.get(url)
+        if response.status_code == 200:
+            await ctx.send(f'Current poll: {response.text}')
+            print(f'Poll recovered with value {response.text} | Status code: {response.status_code}')
+        else:
+            await ctx.send(f'No current poll!')
+            print(f'Poll not found | Status code: {response.status_code}')
+        print(f'{url}')
 
 load_dotenv()
 token = environ['ACCESS_TOKEN']
-bot = Bot(token)
+botPrefix = environ['BOT_PREFIX']
+initChannels = environ['INITIAL_CHANNELS'].split()
+bot = Bot(token, botPrefix, initChannels)
 bot.run()
 # bot.run() is blocking and will stop execution of any below code here until stopped or closed.
